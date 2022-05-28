ef2ada2803ee3e1a986a3d5f9158f0a7532c7d05
AaryaGarg123 <90965186+AaryaGarg123@users.noreply.github.com>
2022-05-28 13:04:22 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,35 @@
+img = "";
+status = "";
+
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function preload() {
+    img = loadImage("download.jpg");
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#00FF00");
+    text("Bed", 45, 75);
+    noFill();
+    stroke("#00FF00");
+    rect(30, 60, 450, 350);
+}
+
+function modelLoded() {
+    console.log("Model Loded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+}
